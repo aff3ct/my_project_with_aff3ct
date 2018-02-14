7dce2d73b19c58eb0d0b24f7fa07b025fcb43e7d
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-02-14 17:43:01 +0100

    Rm multi-threaded compilation.

--- ci/build-mac-osx-clang.sh
+++ ci/build-mac-osx-clang.sh
@@ -6,9 +6,8 @@ examples=(bootstrap tasks factory)
 function compile {
 	mkdir build_test
 	cd build_test
-	cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -funroll-loops -march=native -Wno-deprecated-declarations"
-	THREADS=$(grep -c ^processor /proc/cpuinfo)
-	make -j $THREADS
+	cmake .. -G"Unix Makefiles" -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -funroll-loops -march=native"
+	make
 	cd ..
 }
 
