7431cafeaff97962822a30b43786703d5d49d8fc
Adrien Cassagne <adrien.cassagne@inria.fr>
2018-06-07 23:12:57 +0200

    Fix wrong tags.

--- ci/build-windows-gcc.bat
+++ ci/build-windows-gcc.bat
@@ -18,7 +18,7 @@ exit /B %ERRORLEVEL%
 :compile
 mkdir build_windows_gcc
 cd build_windows_gcc
-cmake .. -G"MinGW Makefiles" -DCMAKE_CXX_COMPILER=g++.exe -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -Wno-misleading-indentation -Wno-deprecated-declarations -funroll-loops -msse4.2"
+cmake .. -G"MinGW Makefiles" -DCMAKE_CXX_COMPILER=g++.exe -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-Wall -Wno-misleading-indentation -Wno-deprecated-declarations -funroll-loops -mavx"
 if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
 mingw32-make
 if %ERRORLEVEL% neq 0 exit /B %ERRORLEVEL%
